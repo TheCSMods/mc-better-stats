d821775922a69966b64fbbf3c311527771661cab
TheCSDev <thecsdev@gmail.com>
2022-09-11 09:08:41 +0200

    Removed unfinished project
    The project is not yet finished, so it doesn't need to be there.

--- betterstats-forge-1.18.2/src/main/java/thecsdev/fabric2forge/bss_p758/fabricapi/DedicatedServerModInitializer.java
+++
@@ -1,6 +0,0 @@
-package thecsdev.fabric2forge.bss_p758.fabricapi;
-
-public interface DedicatedServerModInitializer
-{
-	public void onInitializeServer();
-}
--- betterstats-forge-1.18.2/src/main/java/thecsdev/fabric2forge/bss_p758/text/LiteralText.java
+++
@@ -1,8 +0,0 @@
-package thecsdev.fabric2forge.bss_p758.text;
-
-import net.minecraft.network.chat.TextComponent;
-
-public class LiteralText extends TextComponent
-{
-	public LiteralText(String text) { super(text); }
-}
