70965ec39388726844285b64fe9af436c1be37aa
TheCSDev <thecsdev@gmail.com>
2023-03-03 17:47:19 +0100

    Release v2.3-fabric
    If play-testing passes, this mod version will be released. This mod is now using Architectury API instead of Mixin-s.

--- betterstats-2-fabric-1.18.2/src/main/java/io/github/thecsdev/betterstats/client/BetterStatsClient.java
+++ betterstats-2-fabric-1.18.2/src/main/java/io/github/thecsdev/betterstats/client/BetterStatsClient.java
@@ -2,15 +2,23 @@ package io.github.thecsdev.betterstats.client;
 
 import static io.github.thecsdev.tcdcommons.api.util.TextUtils.translatable;
 
+import dev.architectury.event.EventResult;
 import io.github.thecsdev.betterstats.BetterStats;
 import io.github.thecsdev.betterstats.client.gui.screen.BetterStatsScreen;
+import io.github.thecsdev.betterstats.client.gui_hud.screen.BetterStatsHudScreen;
+import io.github.thecsdev.betterstats.client.gui_hud.screen.BshsAutoRequest;
 import io.github.thecsdev.tcdcommons.api.client.events.screen.TGameMenuScreenEvent;
 import io.github.thecsdev.tcdcommons.api.client.hooks.TGuiHooks;
+import io.github.thecsdev.tcdcommons.api.events.TNetworkEvent;
 import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.Drawable;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.network.NetworkSide;
+import net.minecraft.network.packet.c2s.play.HandSwingC2SPacket;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket.LookAndOnGround;
+import net.minecraft.network.packet.s2c.play.StatisticsS2CPacket;
 import net.minecraft.text.Text;
 
 public final class BetterStatsClient extends BetterStats implements ClientModInitializer
@@ -37,8 +45,47 @@ public final class BetterStatsClient extends BetterStats implements ClientModIni
 				});
 			});
 		});
+		
+		//MixinClientConnection - Tracking packets
+		TNetworkEvent.SEND_PACKET_PRE.register((packet, netSide) ->
+		{
+			//ensure client-side for both physical and logical side
+			if(!BetterStats.isClient() || netSide != NetworkSide.CLIENTBOUND)
+				return EventResult.pass();
+			
+			//---------- track packets sent to the server (C2S)
+			//BshsAutoRequest flag handling
+			if(packet instanceof LookAndOnGround)
+				BshsAutoRequest.flag_moved = true;
+			else if (packet instanceof HandSwingC2SPacket)
+				BshsAutoRequest.flag_handSwung = true;
+			
+			//return
+			return EventResult.pass();
+		});
+		TNetworkEvent.RECEIVE_PACKET_PRE.register((packet, netSide) ->
+		{
+			//ensure client-side for both physical and logical side
+			if(!BetterStats.isClient() || netSide != NetworkSide.CLIENTBOUND)
+				return EventResult.pass();
+			
+			//---------- track the packets sent here from the server (S2C)
+			//update stats hud when receiving statistics
+			if(packet instanceof StatisticsS2CPacket)
+			{
+				var bshs = BetterStatsHudScreen.getInstance(); //do not create new instances
+				if(bshs != null && bshs.flag_tickChildren < 1)
+					bshs.flag_tickChildren = 5; //schedule the widget update
+			}
+			
+			//return
+			return EventResult.pass();
+		});
 	}
 	// ==================================================
+	/**
+	 * Used to target a {@link ButtonWidget} in a given {@link Screen}.
+	 */
 	private ButtonWidget betterstats_snipeButton(Screen screen, Text buttonText)
 	{
 		String btnTxtStr = buttonText.getString();
--- betterstats-2-fabric-1.19.3/src/main/java/io/github/thecsdev/betterstats/client/BetterStatsClient.java
+++ betterstats-2-fabric-1.19.3/src/main/java/io/github/thecsdev/betterstats/client/BetterStatsClient.java
@@ -2,15 +2,26 @@ package io.github.thecsdev.betterstats.client;
 
 import static io.github.thecsdev.tcdcommons.api.util.TextUtils.translatable;
 
+import java.util.List;
+
+import dev.architectury.event.EventResult;
 import io.github.thecsdev.betterstats.BetterStats;
 import io.github.thecsdev.betterstats.client.gui.screen.BetterStatsScreen;
+import io.github.thecsdev.betterstats.client.gui_hud.screen.BetterStatsHudScreen;
+import io.github.thecsdev.betterstats.client.gui_hud.screen.BshsAutoRequest;
 import io.github.thecsdev.tcdcommons.api.client.events.screen.TGameMenuScreenEvent;
 import io.github.thecsdev.tcdcommons.api.client.hooks.TGuiHooks;
+import io.github.thecsdev.tcdcommons.api.events.TNetworkEvent;
 import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.Drawable;
+import net.minecraft.client.gui.Element;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.GridWidget;
+import net.minecraft.network.NetworkSide;
+import net.minecraft.network.packet.c2s.play.HandSwingC2SPacket;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket.LookAndOnGround;
+import net.minecraft.network.packet.s2c.play.StatisticsS2CPacket;
 import net.minecraft.text.Text;
 
 public final class BetterStatsClient extends BetterStats implements ClientModInitializer
@@ -29,7 +40,6 @@ public final class BetterStatsClient extends BetterStats implements ClientModIni
 				if(ogStatsBtn == null) return;
 				
 				//replace it's function
-				//((MixinButtonWidget)ogStatsBtn).setOnPress(btn ->
 				TGuiHooks.setButtonPressAction(ogStatsBtn, btn ->
 				{
 					final MinecraftClient client = MinecraftClient.getInstance();
@@ -37,21 +47,73 @@ public final class BetterStatsClient extends BetterStats implements ClientModIni
 				});
 			});
 		});
+		
+		//MixinClientConnection - Tracking packets
+		TNetworkEvent.SEND_PACKET_PRE.register((packet, netSide) ->
+		{
+			//ensure client-side for both physical and logical side
+			if(!BetterStats.isClient() || netSide != NetworkSide.CLIENTBOUND)
+				return EventResult.pass();
+			
+			//---------- track packets sent to the server (C2S)
+			//BshsAutoRequest flag handling
+			if(packet instanceof LookAndOnGround)
+				BshsAutoRequest.flag_moved = true;
+			else if (packet instanceof HandSwingC2SPacket)
+				BshsAutoRequest.flag_handSwung = true;
+			
+			//return
+			return EventResult.pass();
+		});
+		TNetworkEvent.RECEIVE_PACKET_PRE.register((packet, netSide) ->
+		{
+			//ensure client-side for both physical and logical side
+			if(!BetterStats.isClient() || netSide != NetworkSide.CLIENTBOUND)
+				return EventResult.pass();
+			
+			//---------- track the packets sent here from the server (S2C)
+			//update stats hud when receiving statistics
+			if(packet instanceof StatisticsS2CPacket)
+			{
+				var bshs = BetterStatsHudScreen.getInstance(); //do not create new instances
+				if(bshs != null && bshs.flag_tickChildren < 1)
+					bshs.flag_tickChildren = 5; //schedule the widget update
+			}
+			
+			//return
+			return EventResult.pass();
+		});
 	}
 	// ==================================================
 	private ButtonWidget betterstats_snipeButton(Screen screen, Text buttonText)
+	{
+		return betterstats_snipeButton(buttonText,
+				dev.architectury.hooks.client.screen.ScreenHooks.getNarratables(screen)
+				.stream()
+				.map(e -> (Element)e)
+				.toList());
+	}
+	
+	@SuppressWarnings("unchecked")
+	private ButtonWidget betterstats_snipeButton(Text buttonText, List<Element> elements)
 	{
 		String btnTxtStr = buttonText.getString();
 		ButtonWidget foundBtn = null;
 		
 		//iterate all drawables
-		//for(Drawable drawable : ((MixinScreen)(Object)this).getDrawables())
-		for(Drawable drawable : dev.architectury.hooks.client.screen.ScreenHooks.getRenderables(screen))
+		for(Element selectable : elements)
 		{
+			//check grids
+			if(selectable instanceof GridWidget)
+			{
+				GridWidget grid = (GridWidget)selectable;
+				return betterstats_snipeButton(buttonText, (List<Element>)grid.children());
+			}
+			
 			//ignore non-buttons
-			if(!(drawable instanceof ButtonWidget))
+			if(!(selectable instanceof ButtonWidget))
 				continue;
-			ButtonWidget btn = (ButtonWidget)drawable;
+			ButtonWidget btn = (ButtonWidget)selectable;
 			
 			//compare texts
 			if(!btnTxtStr.equals(btn.getMessage().getString()))
@@ -61,6 +123,7 @@ public final class BetterStatsClient extends BetterStats implements ClientModIni
 			foundBtn = btn;
 		}
 		
+		//return the button if found
 		return foundBtn;
 	}
 	// ==================================================
