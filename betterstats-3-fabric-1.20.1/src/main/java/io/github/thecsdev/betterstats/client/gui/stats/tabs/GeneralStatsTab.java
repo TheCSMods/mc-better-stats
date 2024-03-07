65698197dd2e12678293018db6f01f24b484ebb8
Dave <thecsdev@gmail.com>
2024-03-07 17:50:39 +0100

    v3.9.2 dev. progress
    General stats now support grouping

--- betterstats-3-fabric-1.20.1/src/main/java/io/github/thecsdev/betterstats/client/gui/stats/tabs/GeneralStatsTab.java
+++ betterstats-3-fabric-1.20.1/src/main/java/io/github/thecsdev/betterstats/client/gui/stats/tabs/GeneralStatsTab.java
@@ -2,7 +2,6 @@ package io.github.thecsdev.betterstats.client.gui.stats.tabs;
 
 import static io.github.thecsdev.betterstats.api.client.gui.util.StatsTabUtils.FILTER_ID_SORT_CUSTOMS;
 import static io.github.thecsdev.betterstats.api.client.gui.util.StatsTabUtils.GAP;
-import static io.github.thecsdev.betterstats.api.util.stats.SUGeneralStat.getGeneralStats;
 import static io.github.thecsdev.betterstats.client.BetterStatsClient.MC_CLIENT;
 import static io.github.thecsdev.betterstats.client.gui.stats.panel.StatsTabPanel.TXT_NO_STATS_YET;
 import static io.github.thecsdev.tcdcommons.api.client.gui.config.TConfigPanelBuilder.nextPanelVerticalRect;
@@ -10,7 +9,9 @@ import static io.github.thecsdev.tcdcommons.api.hooks.world.biome.source.BiomeAc
 import static io.github.thecsdev.tcdcommons.api.util.TextUtils.literal;
 import static io.github.thecsdev.tcdcommons.api.util.TextUtils.translatable;
 
+import java.util.Collection;
 import java.util.Objects;
+import java.util.function.Consumer;
 
 import org.jetbrains.annotations.ApiStatus.Internal;
 
@@ -19,14 +20,17 @@ import io.github.thecsdev.betterstats.api.client.gui.stats.panel.GameProfilePane
 import io.github.thecsdev.betterstats.api.client.gui.stats.widget.CustomStatElement;
 import io.github.thecsdev.betterstats.api.client.gui.stats.widget.GeneralStatWidget;
 import io.github.thecsdev.betterstats.api.client.gui.util.StatsTabUtils;
+import io.github.thecsdev.betterstats.api.util.enumerations.FilterGroupBy;
 import io.github.thecsdev.betterstats.api.util.enumerations.FilterSortCustomsBy;
 import io.github.thecsdev.betterstats.api.util.stats.SUGeneralStat;
 import io.github.thecsdev.betterstats.client.gui.screen.hud.BetterStatsHudScreen;
 import io.github.thecsdev.betterstats.client.gui.screen.hud.entry.StatsHudGeneralEntry;
 import io.github.thecsdev.betterstats.util.BST;
+import io.github.thecsdev.tcdcommons.api.client.gui.config.TConfigPanelBuilder;
 import io.github.thecsdev.tcdcommons.api.client.gui.other.TFillColorElement;
 import io.github.thecsdev.tcdcommons.api.client.gui.other.TLabelElement;
 import io.github.thecsdev.tcdcommons.api.client.gui.panel.TPanelElement;
+import io.github.thecsdev.tcdcommons.api.util.annotations.Virtual;
 import io.github.thecsdev.tcdcommons.api.util.enumerations.HorizontalAlignment;
 import net.minecraft.text.Text;
 
@@ -35,11 +39,6 @@ public final @Internal class GeneralStatsTab extends BSStatsTab<SUGeneralStat>
 	// ==================================================
 	public final @Override Text getName() { return translatable("stat.generalButton"); }
 	// --------------------------------------------------
-	protected final @Override void initExtraFilters(FiltersInitContext initContext)
-	{
-		StatsTabUtils.initSortCustomsByFilter(initContext);
-	}
-	// ==================================================
 	public final @Override void initStats(final StatsInitContext initContext)
 	{
 		//obtain initialization info
@@ -50,78 +49,143 @@ public final @Internal class GeneralStatsTab extends BSStatsTab<SUGeneralStat>
 		final var filterSettings = initContext.getFilterSettings();
 		
 		//obtain statistics and sort them
-		final var stats = getGeneralStats(statsProvider, getPredicate(filterSettings));
+		final var groupBy = filterSettings.getPropertyOrDefault(StatsTabUtils.FILTER_ID_GROUP, FilterGroupBy.DEFAULT);
 		final var sortBy = filterSettings.getProperty(FILTER_ID_SORT_CUSTOMS, FilterSortCustomsBy.DEFAULT);
-		if(sortBy != null) sortBy.sortGeneralStats(stats);
 		
 		// ---------- initialize gui
 		//game profile panel
 		final var panel_gp = new GameProfilePanel(sp, sp, panel.getWidth() - (sp*2), statsProvider);
 		panel.addChild(panel_gp, true);
 		
-		//world statistics
-		if(BetterStatsConfig.DEBUG_MODE)
-		{
-			StatsTabUtils.initGroupLabel(panel, translatable("createWorld.tab.world.title"))
-				.setTextHorizontalAlignment(HorizontalAlignment.CENTER);
-			final var n1 = nextPanelVerticalRect(panel);
-			final var wsp = new TFillColorElement(n1.x, n1.y, n1.width, (CustomStatElement.HEIGHT * 2) + GAP);
-			wsp.setColor(TPanelElement.COLOR_BACKGROUND);
-			panel.addChild(wsp, false);
-			
-			final var world = MC_CLIENT.world;
-			final var ws1 = new CustomStatElement(
-					wsp.getX(), wsp.getY(), wsp.getWidth(),
-					translatable("selectWorld.enterName"),
-					literal(Objects.toString((world != null) ? world.getRegistryKey().getValue() : "-"))
-				);
-			final var ws2 = new CustomStatElement(
-					wsp.getX(), wsp.getEndY() - CustomStatElement.HEIGHT, wsp.getWidth(),
-					BST.sTab_hashedSeed(),
-					literal(Objects.toString(getBiomeAccessSeed(world.getBiomeAccess())))
-				);
-			wsp.addChild(ws1, false);
-			wsp.addChild(ws2, false);
-		}
+		// ---------- "debug mode"-specific statistics
+		if(BetterStatsConfig.DEBUG_MODE) initDebugInfo(panel); 
 		
-		//general statistics
-		StatsTabUtils.initGroupLabel(panel, translatable("entity.minecraft.player"))
-			.setTextHorizontalAlignment(HorizontalAlignment.CENTER);
-		int nextX = sp;
-		int nextY = (nextPanelVerticalRect(panel).y - panel.getY()) + GAP;
-		int nextW = panel.getWidth() - (sp * 2);
-		if(stats.size() > 0)
+		// ---------- general statistics
+		//player stats label
+		if(BetterStatsConfig.DEBUG_MODE)
+			StatsTabUtils.initGroupLabel(panel, translatable("entity.minecraft.player"))
+				.setTextHorizontalAlignment(HorizontalAlignment.CENTER);
+		
+		//obtain and sort general stats
+		final var statGroups = (groupBy == FilterGroupBy.DEFAULT) ?
+				getDefaultGroupFilter().apply(SUGeneralStat.getGeneralStats(statsProvider, getPredicate(filterSettings))) :
+				groupBy.apply(SUGeneralStat.getGeneralStats(statsProvider, getPredicate(filterSettings)));
+		statGroups.entrySet().forEach(e -> sortBy.sortGeneralStats(e.getValue()));
+		
+		//init general stats
+		if(statGroups.size() > 0) for(final var statGroup : statGroups.entrySet())
 		{
-			for(final SUGeneralStat stat : stats)
-			{
-				final var statWidget = new GeneralStatWidget(nextX, nextY, nextW, stat);
-				panel.addChild(statWidget, true);
-				nextY += statWidget.getHeight() + GAP;
-				
-				statWidget.eContextMenu.register((__, cMenu) ->
-				{
-					cMenu.addButton(BST.hud_pinStat(), ___ ->
-					{
-						final var hud = BetterStatsHudScreen.getInstance();
-						hud.setParentScreen(MC_CLIENT.currentScreen);
-						hud.addEntry(new StatsHudGeneralEntry(statWidget.getStat()));
-						MC_CLIENT.setScreen(hud.getAsScreen());
-					});
-					cMenu.addButton(translatable("mco.selectServer.close"), ___ -> {});
-				});
-			}
+			final var group = statGroup.getKey();
+			StatsTabUtils.initGroupLabel(panel, group != null ? group : literal("*"));
+			initStats(panel, statGroup.getValue(), widget -> processWidget(widget));
 		}
-		else
+		else //init "no stats" label
 		{
-			final var fill = new TFillColorElement(nextX, nextY, nextW, GeneralStatWidget.HEIGHT);
-			fill.setColor(TPanelElement.COLOR_BACKGROUND);
-			panel.addChild(fill, true);
+			//obtain the next XYWH
+			final var n1 = TConfigPanelBuilder.nextPanelVerticalRect(panel);
 			
+			//create and add the element
+			final var fill = new TFillColorElement(n1.x, n1.y + GAP, n1.width, GeneralStatWidget.HEIGHT);
+			fill.setColor(TPanelElement.COLOR_BACKGROUND);
+			panel.addChild(fill, false);
+			
+			//create and add the element's label
 			final var lbl_noStats = new TLabelElement(0, 0, fill.getWidth(), fill.getHeight());
 			lbl_noStats.setText(TXT_NO_STATS_YET);
 			lbl_noStats.setTextHorizontalAlignment(HorizontalAlignment.CENTER);
 			fill.addChild(lbl_noStats, true);
 		}
+	}
+	// --------------------------------------------------
+	protected final @Override void initExtraFilters(FiltersInitContext initContext)
+	{
+		StatsTabUtils.initGroupByFilter(initContext);
+		StatsTabUtils.initSortCustomsByFilter(initContext);
+	}
+	// ==================================================
+	/**
+	 * Returns the {@link FilterGroupBy} that'll be used by "default".
+	 * @apiNote Must not return {@code null}.
+	 */
+	protected @Virtual FilterGroupBy getDefaultGroupFilter() { return FilterGroupBy.DEFAULT; }
+	// --------------------------------------------------
+	/**
+	 * Initializes a {@link Collection} of {@link SUGeneralStat}s onto a {@link TPanelElement}.
+	 * @param panel The {@link TPanelElement}.
+	 * @param stats The {@link SUGeneralStat}s to initialize.
+	 * @param processWidget Optional {@link Consumer} that allows you to make changes to widgets as they are created.
+	 */
+	protected final void initStats(
+			TPanelElement panel,
+			Collection<SUGeneralStat> stats,
+			Consumer<GeneralStatWidget> processWidget)
+	{
+		//iterate all general stats that are to be added
+		for(final SUGeneralStat stat : stats)
+		{
+			//calculate the next XYWH
+			final var n1 = TConfigPanelBuilder.nextPanelVerticalRect(panel);
+			
+			//create and add the stat widget
+			final var statWidget = new GeneralStatWidget(n1.x, n1.y + GAP, n1.width, stat);
+			panel.addChild(statWidget, false);
+			
+			//process the stat widget
+			if(processWidget != null)
+				processWidget.accept(statWidget);
+		}
+	}
+	//
+	/**
+	 * Default {@link GeneralStatWidget} processing logic.<br/>
+	 * Primarily used for {@link #initStats(TPanelElement, Collection, Consumer)}
+	 * @param widget The {@link GeneralStatWidget}.
+	 */
+	protected @Virtual void processWidget(GeneralStatWidget widget)
+	{
+		widget.eContextMenu.register((__, cMenu) ->
+		{
+			cMenu.addButton(BST.hud_pinStat(), ___ ->
+			{
+				final var hud = BetterStatsHudScreen.getInstance();
+				hud.setParentScreen(MC_CLIENT.currentScreen);
+				hud.addEntry(new StatsHudGeneralEntry(widget.getStat()));
+				MC_CLIENT.setScreen(hud.getAsScreen());
+			});
+			cMenu.addButton(translatable("mco.selectServer.close"), ___ -> {});
+		});
+	}
+	// --------------------------------------------------
+	/**
+	 * Initializes "debug mode" info.
+	 * @param panel The {@link TPanelElement} to initialize to.
+	 */
+	protected static final void initDebugInfo(TPanelElement panel)
+	{
+		//init the group label
+		StatsTabUtils.initGroupLabel(panel, translatable("createWorld.tab.world.title"))
+			.setTextHorizontalAlignment(HorizontalAlignment.CENTER);
+		
+		//init the "world stat panel"
+		final var n1 = nextPanelVerticalRect(panel);
+		final var wsp = new TFillColorElement(n1.x, n1.y, n1.width, (CustomStatElement.HEIGHT * 2) + GAP);
+		wsp.setColor(TPanelElement.COLOR_BACKGROUND);
+		panel.addChild(wsp, false);
+		
+		//init the world name and seed gui stats
+		final var world = MC_CLIENT.world;
+		final var ws1 = new CustomStatElement(
+			wsp.getX(), wsp.getY(), wsp.getWidth(),
+			translatable("selectWorld.enterName"),
+			literal(Objects.toString((world != null) ? world.getRegistryKey().getValue() : "-"))
+		);
+		final var ws2 = new CustomStatElement(
+			wsp.getX(), wsp.getEndY() - CustomStatElement.HEIGHT, wsp.getWidth(),
+			BST.sTab_hashedSeed(),
+			literal(Objects.toString(getBiomeAccessSeed(world.getBiomeAccess())))
+		);
+		wsp.addChild(ws1, false);
+		wsp.addChild(ws2, false);
 	}
 	// ==================================================
 }
